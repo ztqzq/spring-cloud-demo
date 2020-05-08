5cc5b043f2a7191e76ccb368fbfe00c976cc72bf
1093043163@qq.com <1093043163@qq.com>
2020-05-08 19:00:52 +0800

    1

--- spring-cloud-web/src/main/java/com/qzq/MyController.java
+++ spring-cloud-web/src/main/java/com/qzq/MyController.java
@@ -1,10 +1,14 @@
 package com.qzq;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MyController {
@@ -12,11 +16,22 @@ public class MyController {
     public String index() {
         return "index";
     }
+
+    @Bean
+    //实现负载均衡的注解
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
 
 @RestController
 @RefreshScope
 class MyController2 {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Value("${webapp}")
     private String info;
 
@@ -26,8 +41,17 @@ class MyController2 {
     @Value("${name}")
     private String info3;
 
+    @Value("${server.port}")
+    private String port;
+
     @RequestMapping("/get")
     public String get() {
-        return info + "\n" + info2 + "\n" + info3;
+        return info + "\r\n" + info2 + "\r\n" + info3 + "\r\n" + port;
+    }
+
+    @RequestMapping("/get2")
+    public String get2(){
+        String forObject = restTemplate.getForObject("http://webapp2/get", String.class);
+        return forObject;
     }
 }
